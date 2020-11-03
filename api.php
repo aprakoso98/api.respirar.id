6962dbd7694053d7ffdd4604d0cd3c0264d62032
aprakoso98 <adhyt.scott@gmail.com>
2020-11-03 22:44:10 +0700

    fix manae info & data null

--- api.php
+++ api.php
@@ -2,15 +2,15 @@
 error_reporting(0);
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-// header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-// header("Access-Control-Allow-Credentials: true");
-// header("Access-Control-Max-Age: 86400");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 86400");
 
 $path = "";
-$filename = $path . "./config.json";
-$filename = file_exists($filename) ? $filename : $path . "/config-prod.json";
-$GLOBALS['config'] = json_decode(file_get_contents($filename));
-// $GLOBALS['config'] = json_decode('{"db": {"host": "localhost","user": "revtauwebUser","pass": "revtauweb2020#pass","selectdb": "revtauweb_DB"}}');
+$dev = '{"host":"localhost","user":"root","pass":"","selectdb":"respirar_data"}';
+$prod = '{"host":"localhost","user":"respirar_data","pass":"R3sp1r4r.1d","selectdb":"respirar_data"}';
+$GLOBALS['config'] = json_decode(sprintf('{"db": %s}', $dev));
+// $GLOBALS['config'] = json_decode(sprintf('{"db": %s}', $prod));
 
 require $path . "./php-main/main.php";
 
@@ -21,14 +21,6 @@ $db = new CRUD();
 
 require $path . "./endpoints.php";
 
-$time = new TimeUtil();
-$now = $time->getTimeStamp();
-if (intval($now) >= 1583584804) {
-	// important();
-}
-
 if (!$debug) {
-	$logger = new Logger();
-	$logger->log(jsonify($response));
 	echo jsonify($response);
 }
